f5b5838c610b237df3693645a7f2c58980c642c1
CamiloAangel <angelcami0912@gmail.com>
2025-02-16 11:53:14 -0500

    Update

--- prueba.py
+++ prueba.py
@@ -1,4 +1,19 @@
 
 print("Primer comentario Camilo.")
 print("No, hoy es domingo")
-print("Soy Alan")
+print("Soy Alan y yo Camilo jeje")
+
+def fibonacci_iterativo(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib = [0, 1]
+    for i in range(1, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+print(fibonacci_iterativo(10))
